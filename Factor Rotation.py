eb72bf43e1c9f23f139eb8cb40da3c4e4950e08e
djaggi <jagg@zhaw.ch>
2022-02-21 14:15:37 +0100

    Update factor rotation to accomodate new environment

--- Factor Rotation.py
+++ Factor Rotation.py
@@ -1,20 +1,34 @@
 # %%
 import numpy as np
-from src.data.data_feed import BaseDataFeed
-from src.environment import BaseEnvironment
+from src.data.rebalancing_schedule import PeriodicSchedule
+from src.data.data_feed import CSVDataFeed
+from src.env.base_env import BaseEnv
+import matplotlib.pyplot as plt
 
 # %%
-# Input parameters
-START_DATE = '2020-01-02'               # start date of simulation
-END_DATE = '2020-12-31'                 # end date of simulation
-initial_balance = 10000                 # cash to be invested
-benchmark_wgts = np.array([0.5, 0.5])   # benchmark weights
+feed = CSVDataFeed(file_name="example_data.csv")
 
-n_sample = 252                          # number of dates to be generated
-dt = 1 / 252                            # time delta
+env_config = {
+    "initial_balance": 10000,
+    "benchmark_type": "custom",
+    "benchmark_wgts": np.array([0.5, 0.5]),
+    "start_date": "2018-12-31",
+    "end_date": "2020-12-31",
+    "busday_offset_start": 250,
+    "cost_pct": 0.0005,
+    "reward_scaling": 1,
+    "obs_price_hist": 5,
+}
+
+# now try a different rebalancing frequency...
+schedule = PeriodicSchedule(frequency="WOM-3FRI")
 # %%
-data_feed = BaseDataFeed(["AAPL","MSFT"], START_DATE, END_DATE)
-# %%
-trading_env = BaseEnvironment(data_feed, initial_balance)
-# %%
-model = 
+env = BaseEnv(data_feed=feed, config=env_config, rebalance_schedule=schedule)
+obs = env.reset()
+done = False
+while not done:
+    action = env.action_space.sample()
+    # action = np.array([-1, 1])
+    obs, rew, done, _ = env.step(action)
+env.plot_current_performance()
+plt.show()
