a20196a22d8e0d13aab632fc37936b14ea1d74bf
djaggi <jagg@zhaw.ch>
2022-03-15 16:18:35 +0100

    added indicators and indicator pipeline to the script

---
+++ src/env/indicator.py
@@ -0,0 +1,32 @@
+class IndicatorPipeline(object):
+    def __init__(self):
+        self.indicators = []
+        pass
+
+    def add_indicator(self, indicator):
+        self.indicators.append(indicator)
+        pass
+
+
+class BaseIndicator(object):
+    def __init__(self):
+        pass
+
+    def calc(self, instance, prices):
+        return prices
+
+
+class MovingAverage(BaseIndicator):
+    def __init__(self, window):
+        self.window = window
+
+    def calc(self, instance, prices):
+        return prices.rolling(self.window).mean()
+
+
+if __name__ == '__main__':
+    indicator_pipeline = IndicatorPipeline()
+    base_indicator = BaseIndicator()
+
+    for indicator in indicator_pipeline.indicators:
+        indicator.calc()
