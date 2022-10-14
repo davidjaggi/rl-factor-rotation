ddd55bea49054dcaaba69ec5446e03b284fb4c57
djaggi <jagg@zhaw.ch>
2022-10-14 09:31:34 +0200

    push wip progress

--- test/test_broker.py
+++ test/test_broker.py
@@ -1,8 +1,6 @@
 import random
 import unittest
 
-import numpy as np
-
 from src.data.feed import CSVDataFeed
 from src.data.rebalancing_schedule import PeriodicSchedule
 from src.env.broker import Broker
@@ -11,18 +9,6 @@ from src.utils.load_path import load_data_path
 
 DATA_PATH = load_data_path()
 
-ENV_CONFIG = {
-    "initial_balance": 10000,
-    "initial_weights": np.array([0.5, 0.5]),
-    "benchmark_type": "custom",
-    "benchmark_wgts": np.array([0.5, 0.5]),
-    "start_date": "2018-12-31",
-    "end_date": "2020-12-31",
-    "busday_offset_start": 250,
-    "cost_pct": 0.0005,
-    "reward_scaling": 1,
-    "obs_price_hist": 5,
-}
 
 
 class TestBroker(unittest.TestCase):
@@ -32,7 +18,33 @@ class TestBroker(unittest.TestCase):
             DATA_PATH + "/example_data.csv"
         )
         self.rebalancing_schedule = PeriodicSchedule(frequency="WOM-3FRI")
-        self.broker = Broker(self.feed)
+        config = {
+            "rl_portfolio": {
+                'name': 'rl_portfolio',
+                'type': None,
+                'initial_balance': 1000,
+                'initial_weights': [0.5, 0.5],
+                'restrictions': dict(),
+                'start_date': '2020-02-24',
+                'schedule': self.rebalancing_schedule
+            },
+            "benchmark_portfolio": {
+                'name': 'benchmark_portfolio',
+                'type': "equally_weighted",
+                'initial_balance': 1000,
+                'initial_weights': [0.5, 0.5],
+                'restrictions': dict(),
+                'start_date': '2020-02-24',
+                'schedule': self.rebalancing_schedule
+            },
+            "start_date": "2018-12-31",
+            "end_date": "2020-12-31",
+            "busday_offset_start": 250,
+            "cost_pct": 0.0005,
+            "reward_scaling": 1,
+            "obs_price_hist": 5,
+        }
+        self.broker = Broker(self.feed, config)
 
     def test_reset(self):
         self.broker.reset()
