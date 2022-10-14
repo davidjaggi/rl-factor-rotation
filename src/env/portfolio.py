5cdfcf0435a59004fc784dcf24ae3ed2f3deaea1
djaggi <jagg@zhaw.ch>
2022-10-14 09:21:24 +0200

    update portfolio

--- src/env/portfolio.py
+++ src/env/portfolio.py
@@ -1,30 +1,32 @@
+from abc import ABC
+
 from pandas.tseries.offsets import BMonthEnd
 
 offset = BMonthEnd()
 
-from abc import ABC
-
 
 def last_Bday_of_month(dt):
-
     return offset.rollforward(dt)
+
 
 class Portfolio(ABC):
     """ Parent class for all portfolios.
     Args:
-        initial_holdings (dict): Initial holdings of the portfolio, in units of cash and assets.
+        initial_balance (dict): Initial holdings of the portfolio, in units of cash and assets.
         restrictions (list): list of boolean methods to apply to self.ideal_weights when rebalancing.
         start_date (Datetime): Starting date of the portfolio.
     """
 
-    def __init__(self, initial_holdings, restrictions, start_date):
-        #TODO: Do we need any other attributes?
-
-        self.holdings = initial_holdings
-        self.restrictions = restrictions
-        self.start_date = start_date
-        self.dt = start_date
-        self.trade_idx = 0 # Trade Counter for testing
+    def __init__(self, config):
+        self.balance = config["initial_balance"]
+        self.initial_weights = config["initial_weights"]
+        self.restrictions = config["restrictions"]
+        self.start_date = config["start_date"]
+        self.dt = config["start_date"]
+        self.schedule = config["schedule"]
+        self.type = config["type"]
+        self.holdings = []
+        self.trade_idx = 0  # Trade Counter for testing
 
     def reset(self):
         """"
@@ -34,34 +36,30 @@ class Portfolio(ABC):
         self.trade_idx = 0
 
 
-    def rebalancing_schedule(self, date):
-        """
-        Boolean Method that implements the rebalancing schedule, to be implemented by each individual portfolio.
-        """
-        raise NotImplementedError
-
 class BenchmarkPortfolio(Portfolio):
 
     def __init__(self, *args, **kwargs):
         super(BenchmarkPortfolio, self).__init__(*args, **kwargs)
         self.ideal_weights = [0.5, 0.5]
 
-    def rebalancing_schedule(self, date):
-        """
-        The benchmark_portfolios update once a month on the last Business day.
-        """
-        return date == last_Bday_of_month(date)
-
 
 class RLPortfolio(Portfolio):
 
     def __init__(self, *args, **kwargs):
         super(RLPortfolio, self).__init__(*args, **kwargs)
-        self.ideal_weights = [0.5, 0.5]
 
-    def rebalancing_schedule(self, date):
-        """
-        The RL_portfolios update ...
-        """
-        # TODO: Change this if needed
-        return date == last_Bday_of_month(date)
+        if self.type == "equally_weighted":
+            self.ideal_weights = [0.5, 0.5]
+
+
+if __name__ == '__main__':
+    schedule = rebalancing_schedule()
+    portfolio_dict = {
+        'name': 'benchmark_portfolio',
+        'type': "equally_weighted",
+        'initial_balance': 1000,
+        'initial_weights': [0.5, 0.5],
+        'restrictions': dict(),
+        'start_date': '2020-02-24',
+        'schedule': schedule
+    }
