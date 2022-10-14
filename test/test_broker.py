8f091281e55e3312d4ce79a97881d7bd3666aab5
djaggi <jagg@zhaw.ch>
2022-10-14 09:57:45 +0200

    update broker config

--- test/test_broker.py
+++ test/test_broker.py
@@ -16,19 +16,21 @@ class TestBroker(unittest.TestCase):
             DATA_PATH + "/example_data.csv"
         )
         self.rebalancing_schedule = PeriodicSchedule(frequency="WOM-3FRI")
+
         bench_config = {
             'name': 'benchmark_portfolio',
             'type': "equally_weighted",
-            'initial_balance': 1000,
+            'initial_balance': 10000,
             'initial_weights': [0.5, 0.5],
             'restrictions': dict(),
             'start_date': '2020-02-24',
             'schedule': self.rebalancing_schedule
         }
+
         rl_config = {
             'name': 'rl_portfolio',
             'type': None,
-            'initial_balance': 1000,
+            'initial_balance': 10000,
             'initial_weights': [0.5, 0.5],
             'restrictions': dict(),
             'start_date': '2020-02-24',
@@ -41,7 +43,7 @@ class TestBroker(unittest.TestCase):
             "start_date": "2018-12-31",
             "end_date": "2020-12-31",
             "busday_offset_start": 250,
-            "cost_pct": 0.0005,
+            "transaction_cost": 0.0005,
             "reward_scaling": 1,
             "obs_price_hist": 5,
         }
@@ -51,8 +53,5 @@ class TestBroker(unittest.TestCase):
         self.broker.reset()
         pass
 
-    def test_assign_portfolios(self):
-        self.broker.benchmark_portfolio = BenchmarkPortfolio([0.5, 0.5], None, "2000-01-01")
-        self.broker.rl_portfolio = RLPortfolio([0.5, 0.5], None, "2000-01-01")
-        assert self.broker.benchmark_portfolio is not None
-        assert self.broker.rl_portfolio is not None
+    def test_initial_weights(self):
+        pass
