4cf55eadefde0c3e233b6616e717bedb665e1854
djaggi <jagg@zhaw.ch>
2022-10-19 13:57:40 +0200

    update base env

--- src/env/base_env.py
+++ src/env/base_env.py
@@ -185,6 +185,7 @@ class BaseEnv(gym.Env, ABC):
         return [seed]
 
     def _benchmark_weights(self):
+        # TODO: make this a function of the broker
         if self.config["benchmark_type"] == "equally_weighted":
             wgts = np.ones(self.data_feed.num_assets) / self.data_feed.num_assets
         elif self.config["benchmark_type"] == "custom":
@@ -207,6 +208,7 @@ class BaseEnv(gym.Env, ABC):
 
     @property
     def ptf_performance(self):
+        # TODO: make this a fuction of the portfolio
         df = pd.DataFrame(
             list(zip(self.date_memory, self.asset_memory)),
             columns=["Date", "Ptf_Value"],
@@ -215,6 +217,7 @@ class BaseEnv(gym.Env, ABC):
         return df
 
     def plot_current_performance(self):
+        # TODO: make this a fuction of the portfolio
         """Plot current performance of the agent"""
         if self.config["benchmark_type"] is not None:
             dfs = pd.concat([self.bmk_performance, self.ptf_performance], axis=1)
@@ -260,38 +263,7 @@ class BaseEnv(gym.Env, ABC):
 
     def reward_func(self):
         """Reward function for the portfolio. Currently the distance of the portfolio to the benchmark."""
+        # TODO: reward is the difference of the current values vs. the benchmark
         reward = self.current_ptf_values[-1] - self.current_ptf_values_bmk[-1]
         return reward
 
-# if __name__ == "__main__":
-#     from src.data.rebalancing_schedule import PeriodicSchedule
-#     from src.data.feed import CSVDataFeed
-#     import matplotlib.pyplot as plt
-#
-#     feed = CSVDataFeed(file_name="../../data/example_data.csv")
-#
-#     ENV_CONFIG = {
-#         "initial_balance": 10000,
-#         "initial_weights": np.array([0.5, 0.5]),
-#         "benchmark_type": "custom",
-#         "benchmark_wgts": np.array([0.5, 0.5]),
-#         "start_date": "2018-12-31",
-#         "end_date": "2020-12-31",
-#         "busday_offset_start": 250,
-#         "cost_pct": 0.0005,
-#         "reward_scaling": 1,
-#         "obs_price_hist": 5,
-#     }
-#
-#     # now try a different rebalancing frequency...
-#     schedule = PeriodicSchedule(frequency="WOM-3FRI")
-#
-#     env = BaseEnv(data_feed=feed, config=ENV_CONFIG, rebalance_schedule=schedule)
-#     obs = env.reset()
-#     done = False
-#     while not done:
-#         # implement simple agent
-#         action = env.action_space.sample()
-#         obs, rew, done, _ = env.step(action)
-#     env.plot_current_performance()
-#     plt.show()
