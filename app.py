fff66eb3bb28465725d8a91b1e1aba5e09697d5e
djaggi <jagg@zhaw.ch>
2022-11-23 16:37:57 +0100

    enhance: enhance dashboard with columns

--- app.py
+++ app.py
@@ -11,14 +11,17 @@ def run():
     st.set_page_config(page_title="Real-Time RL Dashboard", page_icon="✅", layout="wide")
     # dashboard title
     st.title("RL Dashboard")
-    days = st.text_input(label='Days', value=125)
-    initial_balance = st.text_input(label='Initial balance', value=10000)
-    start_date = st.date_input(label='Start date', value=date(2018, 12, 31))
-    end_date = st.date_input(label='End date', value=date(2020, 12, 31))
-    reward_scaling = st.number_input(label='Reward scaling', value=1, step=1)
-    obs_price_hist = st.number_input(label='Observation price history', value=5, step=1)
-    transaction_cost = float(st.text_input(label='Transaction costs in %', value=0.05)) / 100
-    weighting_method = st.selectbox(label='Weighting method', options=['equally_weighted'])
+    col1, col2 = st.columns(2)
+    with col1:
+        days = st.text_input(label='Days', value=125)
+        initial_balance = st.text_input(label='Initial balance', value=10000)
+        start_date = st.date_input(label='Start date', value=date(2018, 12, 31))
+        end_date = st.date_input(label='End date', value=date(2020, 12, 31))
+    with col2:
+        reward_scaling = st.number_input(label='Reward scaling', value=1, step=1)
+        obs_price_hist = st.number_input(label='Observation price history', value=5, step=1)
+        transaction_cost = float(st.text_input(label='Transaction costs in %', value=0.05)) / 100
+        weighting_method = st.selectbox(label='Weighting method', options=['equally_weighted'])
     training_data = st.selectbox(label='Training data', options=['/example_data.csv'])
 
     if st.button(label='Run Script'):
@@ -39,25 +42,30 @@ def run():
             "text/csv",
             key='download-csv'
         )
+        with col1:
+            st.header('Benchmark holdings')
+            st.line_chart(analyzer.get_positions("benchmark"))
 
-        st.header('Benchmark holdings')
-        st.line_chart(analyzer.get_positions("benchmark"))
+        with col2:
+            st.header('RL Agent holdings')
+            st.line_chart(analyzer.get_positions("rl"))
 
-        st.header('RL Agent holdings')
-        st.line_chart(analyzer.get_positions("rl"))
+        with col1:
+            st.header('Benchmark weights')
+            st.area_chart(analyzer.get_weights("benchmark"))
 
-        st.header('Benchmark weights')
-        st.area_chart(analyzer.get_weights("benchmark"))
-
-        st.header('RL weights')
-        st.area_chart(analyzer.get_weights("rl"))
+        with col2:
+            st.header('RL weights')
+            st.area_chart(analyzer.get_weights("rl"))
 
         st.header('Portfolio value')
         st.line_chart(analyzer.get_values("benchmark"))
 
         st.header('Cash positions')
-        st.line_chart(analyzer.get_cash("benchmark"))
-        st.line_chart(analyzer.get_cash("rl"))
+        with col1:
+            st.line_chart(analyzer.get_cash("benchmark"))
+        with col2:
+            st.line_chart(analyzer.get_cash("rl"))
 
         st.header('Historical asset prices')
         st.line_chart(analyzer.get_prices())
